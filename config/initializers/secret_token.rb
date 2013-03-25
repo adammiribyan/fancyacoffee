e8fe92d176237d2b2d51d88227b4a6cacdb33ba1
Adam Miribyan <adam.miribyan@gmail.com>
2013-03-25 18:49:54 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fancyacoffee::Application.config.secret_key_base = 'ad553cc0430cd1e65c176a603ad74ff790be31922ddc818ef44af624aa9ed490914df7303c58ead9b6a612f9da1a04d2e8b0ae05af1d8b4ea102838a1d56ddfb'
